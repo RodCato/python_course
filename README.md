7bd6e65011444c3e37608a83d6738172e8a2cb80
RodCato <code@eliment.org>
2023-09-17 20:01:22 -0500

    Added Day 1 exercises

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python_course
+
 Master the Python programming language by building 100 unique projects over 100 days.
+
+Day1: Band Name Generator
+
+
---
+++ day1.py
@@ -0,0 +1,17 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+print("Hello world!\nHello world!\nHello world!")
+print("Hello" + " " + "Angela")
+print("Hello" + " " + "Angela\nHello" + " " + "Angela\nHello" + " " + "Angela")
+
+#1.2 Debugging exercise
+#Fix the code below 👇
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#1.3 Input function
+#Write your code below this line 👇
+
