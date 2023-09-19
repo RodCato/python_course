9ff67ac8fec15315e6fcf21668f830b1cfa1afd9
RodCato <code@eliment.org>
2023-09-18 21:59:18 -0500

    completed day 1 of python course

--- day1.py
+++ day1.py
@@ -1,17 +1,58 @@
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
-print("Hello world!\nHello world!\nHello world!")
-print("Hello" + " " + "Angela")
-print("Hello" + " " + "Angela\nHello" + " " + "Angela\nHello" + " " + "Angela")
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+# print("Hello world!\nHello world!\nHello world!")
+# print("Hello" + " " + "Angela")
+# print("Hello" + " " + "Angela\nHello" + " " + "Angela\nHello" + " " + "Angela")
 
-#1.2 Debugging exercise
-#Fix the code below 👇
-print("Day 1 - String Manipulation")
-print('String Concatenation is done with the "+" sign.')
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
+# #1.2 Debugging exercise
+# #Fix the code below 👇
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
 
 #1.3 Input function
 #Write your code below this line 👇
+# 
+
+# print(len (input ("What is your name? ")))
+
+#1.4 Variables
+
+#Write your code below this line 👇
+#
+# name = input("What is your name? ")
+# length = len(name)
+# print(length)
+
+# a = input("a: ")
+# b = input("b: ")
+
+# c = a
+# a = b
+# b = c
+
+# print("a: " + a)
+# print("b: " + b)
+
+#1.5 Band name generator
+
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator.")
+#2. Ask the user for the city that they grew up in.
+city = input("What's name of the city you grew up in?\n")
+#3. Ask the user for the name of a pet.
+pet = input("What's your pet's name?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city + " " + pet)
+#5. Make sure the input cursor shows on a new line, see the example at:
+
+#DAY 1 COMPLETE September 18, 2023
+
+
+
+
+
+
 
